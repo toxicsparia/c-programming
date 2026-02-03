47032df137360c18c9d86164f6b3ffaed5ed650e
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-03 09:33:04 +0530

    New formula appling for making cubic

--- cube maker.c
+++ cube maker.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     float no;
 printf("Enter the number here \n");
 scanf("%f", &no);
-printf("The cube is: %.2f \n", no*no*no);
+printf("The cube is: %.2f \n", pow(no, 3));
     return 0;
 } 
