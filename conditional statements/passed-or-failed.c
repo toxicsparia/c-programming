d4aff067b04116af3fcf1da22c7c33ad803de197
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-08 22:51:44 +0530

    adding a new folder and some practice program

---
+++ conditional statements/passed-or-failed.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int marks;
+    printf("Enter your marks (0-100): \n");
+    if (scanf("%d", &marks) != 1) {
+        printf("Invalid input: expected an integer.\n");
+        return 1;
+    }
+
+    if (marks < 0 || marks > 100) {
+        printf("Invalid mark\n");
+    } else if (marks >= 30) {
+        printf("Passed\n");
+    } else {
+        printf("Failed\n");
+    }
+
+    printf("thank you\n");
+
+    return 0;
+}
