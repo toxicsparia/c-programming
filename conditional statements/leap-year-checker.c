8ca6bf603943fa6b9a4472f791821b151111f072
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-10 23:54:37 +0530

    adding leap year checker

---
+++ conditional statements/leap-year-checker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter the year you wanna check Leap Year or not \n");
+    if(scanf("%d", &year) != 1 || year < 0){ //input validation check
+        printf("This is an invalid input \n");
+        return 1;
+    }
+    if(year % 100 == 00){ //this will detect the century years
+        if(year % 400 == 0){
+            printf("%d is a leap year \n", year);
+        }
+        else{
+            printf("%d isn't a leap year \n", year);
+        }
+        return 1;
+    }
+    if(year % 4 == 0){
+        printf("%d is a leap year \n", year);
+    }
+    else{
+        printf("%d isn't a leap year \n", year);
+    }
+return 0;
+}
