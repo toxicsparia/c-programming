23069c4c6666a31c7acc7e9cf4293ad3bc332c1d
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-17 21:22:59 +0530

    using long long

--- loops/factorial-of-number.c
+++ loops/factorial-of-number.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 int main(){
-    int n, j, x;
+    int n, j;
+    unsigned long long x;
     printf("Enter a number to check the factorial: ");
     if(scanf("%d", &n) !=1){
         printf("Invalid number\n");
         return 1;
     }
     if(n<0){
-        printf("undefind\n");
+        printf("undefined\n");
         return 1;
     }
     j=n;
     x=1;
-    for(int i=1; i<=n; n--){
-        x=x*n;
+    for(int i=1; i<=n; i++){
+        x=x*i;
         }
         printf("%d!= %d\n", j, x);
 return 0;
