8d535179a78999eb4e8111567fe19f392981f455
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-18 16:59:39 +0530

    nasted loop pattern print

---
+++ loops/pattern.c
@@ -0,0 +1,12 @@
+//Search on what a "nested loop" is & print this pattern using it.
+#include <stdio.h>
+
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=5;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+return 0;
+}
