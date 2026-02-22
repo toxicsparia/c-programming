036e927fd8f9311ae48b83e42f6612cc20b1cf21
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-02-22 19:39:41 +0530

    right-angled-triangle-reverse

---
+++ loops/right-angled-triangle-reverse.c
@@ -0,0 +1,18 @@
+/*print this
+*****
+****
+***
+**
+*
+*/
+#include <stdio.h>
+
+int main(){
+    for(int i=5; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
