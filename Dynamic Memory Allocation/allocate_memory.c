da2296ce7cc291c8ac1141d09f03420b2bc5ab6c
toxicsparia <92259843+toxicsparia@users.noreply.github.com>
2026-04-05 22:36:55 +0530

    adding calloc

--- Dynamic Memory Allocation/allocate_memory.c
+++ Dynamic Memory Allocation/allocate_memory.c
@@ -3,10 +3,12 @@
 #include <stdlib.h> // This is (standard library)... we use this for using DMA function like malloc() calloc().
 
 int main(){
+    // memory allocation using malloc()
     float *ptr; // creating a pointer in float
     /*
-    malloc() return void so first we have to specify the data type that we need using (data_type*)
+    malloc() return void so first we have to typecast the data type that we need using (data_type*)
     then we write malloc(how much size we need we can write it in byte or give the predefined size...)
+    DAM - allocate the memory during runtime.. not during compile..
     */
     ptr = (float*) malloc(5*sizeof(float)); // This is the syntex of use malloc.. 
     if(ptr == NULL){
@@ -22,5 +24,22 @@ int main(){
         printf("Price %d = %.2f\n", i+1, ptr[i]);
     }
     free(ptr); // we must have to release the malloc after using it.. using free() function.
+    //memory allocation using calloc()
+    int *cptr; // creating a pointer..
+    /*
+    First typecast. then using calloc (first we have to  specify how much continer we need, then what shize of continer we need.. not like malloc.)
+    calloc give a default value to each continer..
+    */
+    cptr = (int*) calloc(3, sizeof(int));
+    if(cptr == NULL){
+        printf("memory allocation failed\n");
+        return 1;
+    }
+    cptr[0];cptr[1];cptr[2];
+    for(int i=0; i<3; i++){
+        printf("%d\t", cptr[i]);
+    }
+    printf("\n");
+    free(cptr);
     return 0;
 }
